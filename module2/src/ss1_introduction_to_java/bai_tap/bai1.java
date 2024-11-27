8896ac76f1b0ce25de211c10c7c700b690ddf71a
vangoodkid <phamchivan9999@gmail.com>
2024-11-27 20:41:50 +0700

    update

--- module2/src/ss1_introduction_to_java/bai_tap/bai1.java
+++ module2/src/ss1_introduction_to_java/bai_tap/bai1.java
@@ -1,7 +1,12 @@
 package ss1_introduction_to_java.bai_tap;
 
+import java.util.Scanner;
+
 public class bai1 {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name: ");
+        String name = input.nextLine();
+        System.out.println("hello world "+name);
     }
 }
